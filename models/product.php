14685010311aca1a99c128c1f3a68cb1d5fe9386
ROBK1991 <wi20b036@technikum-wien.at>
2022-06-13 18:49:42 +0200

    Products are displayed, subcategories are loaded in products.html Navbar fix

--- models/product.php
+++ models/product.php
@@ -2,5 +2,11 @@
 
 class product
 {
-
+    public $productID;
+    public $productname;
+    public $description;
+    public $price;
+    public $category;
+    public $subcategory;
+    public $image;
 }
